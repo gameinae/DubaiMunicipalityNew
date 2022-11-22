04417c03640add56a6d8390cfdf4bde0cad62108
Xtremezero <mohammedzero43@gmail.com>
2022-11-22 07:37:20 +0400

    Added other districts

--- Assets/_App/Scripts/Map/DistrictSO.cs
+++ Assets/_App/Scripts/Map/DistrictSO.cs
@@ -8,6 +8,6 @@ using UnityEngine;
 public class DistrictSO : ScriptableObject
 {
     public string name;
-    public enum DistrictType { d1, d2, d3, d4 }
+    public enum DistrictType { d1, d2, d3, d4 ,d5, d6, d7, d8 , d9, d10}
     public DistrictType district;
 }
