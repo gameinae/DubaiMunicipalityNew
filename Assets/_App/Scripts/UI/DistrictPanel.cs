644347b03be47032a84e34a3f26960d4c6f029ae
Andrew Gamal <58505236+Andrew-Gamal@users.noreply.github.com>
2022-11-24 09:43:24 +0200

    Side Panel overlapping

--- Assets/_App/Scripts/UI/DistrictPanel.cs
+++ Assets/_App/Scripts/UI/DistrictPanel.cs
@@ -4,21 +4,51 @@ using UnityEngine;
 
 public class DistrictPanel : MonoBehaviour
 {
-    [SerializeField] GameObject[] Middlecharts,BottomCharts, BlockModels;
+    [SerializeField] GameObject[] Middlecharts, BottomCharts, BlockModels;
     [SerializeField] GameObject Cam;
-    public void SwitchChart(int index) 
+    //public void SwitchChart(int index)
+    //{
+    //    for (int i = 0; i < Middlecharts.Length; i++)
+    //    {
+    //        Middlecharts[i].gameObject.SetActive(false);
+    //        BottomCharts[i].gameObject.SetActive(false);
+    //    }
+    //    Middlecharts[index].gameObject.SetActive(true);
+    //    BottomCharts[index].gameObject.SetActive(true);
+
+    //    for (int i = 0; i < BlockModels.Length; i++)
+    //    {
+    //        BlockModels[i].gameObject.SetActive(false);
+    //    }
+    //    BlockModels[index].gameObject.SetActive(true);
+    //    Cam.SetActive(false);
+    //    Cam.SetActive(true);
+    //}
+    public void SwitchCharts(string tag)
     {
-        for (int i = 0; i < Middlecharts.Length; i++) {
+        for (int i = 0; i < Middlecharts.Length; i++)
+        {
             Middlecharts[i].gameObject.SetActive(false);
             BottomCharts[i].gameObject.SetActive(false);
+            if (Middlecharts[i].tag == tag)
+            {
+                Middlecharts[i].gameObject.SetActive(true);
+            }
+            if (BottomCharts[i].tag == tag)
+            {
+                BottomCharts[i].gameObject.SetActive(true);
+            }
         }
-        Middlecharts[index].gameObject.SetActive(true);
-        BottomCharts[index].gameObject.SetActive(true);
 
-        for (int i=0;i< BlockModels.Length;i++) {
+        for (int i = 0; i < BlockModels.Length; i++)
+        {
             BlockModels[i].gameObject.SetActive(false);
+
+            if (BlockModels[i].tag == tag)
+            {
+                BlockModels[i].gameObject.SetActive(true);
+            }
         }
-        BlockModels[index].gameObject.SetActive(true);
         Cam.SetActive(false);
         Cam.SetActive(true);
     }
